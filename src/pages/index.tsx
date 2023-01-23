309c847ab31542f8bd00592527b098027abf33d4
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-01-23 22:29:16 +0900

    🔥

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,7 @@
 import { NextPage } from 'next';
 
-//型推論
-const foo1 = 123;
-
-//型アノテーション
-const double = (x: number): number | undefined => {
-  if (x > 0) {
-    return;
-  }
-  return x * 2;
-};
-
-//型アサーション
-const foo2 = {} as { bar: number };
-foo2.bar = 1;
-
 export const Home: NextPage = () => {
-  return <div>Hello</div>;
+  return <h1>Hello</h1>;
 };
+
+export default Home;

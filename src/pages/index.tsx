e012c2b3a5d30a33ff763e0048de2b6cdd1f76aa
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-01-23 22:55:53 +0900

    🎨 オブジェクトの型定義方法

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,23 @@
 import { NextPage } from 'next';
 
+//オブジェクトの型定義方法
+let obj1: {} = {}; //非推奨
+
+let obj2: object = {}; //非推奨
+
+let obj3: Record<string, Record<'foo', unknown>> = {
+  a: { foo: 1 },
+}; //非推奨 ネストされた状態だと可読性が低くなる
+
+let obj4: { [key: string]: { foo: unknown } } = {
+  a: { foo: 1 },
+}; //推奨
+
+let obj5: { a: number; b: string; foo?: string } = {
+  a: 1,
+  b: 'foo',
+  // foo: 'foo',
+}; //推奨
 export const Home: NextPage = () => {
   return <h1>Hello</h1>;
 };

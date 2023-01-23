1730c9d09141d7ed81417ce7a15fd0263aef6b87
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-01-23 21:47:01 +0900

    🔐 型推論　型アノテーション　型アサーション

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,20 @@
-import styles from '@/styles/Home.module.css';
+import { NextPage } from 'next';
 
-export default function Home() {
-  return <div>test</div>;
-}
+//型推論
+const foo1 = 123;
+
+//型アノテーション
+const double = (x: number): number | undefined => {
+  if (x > 0) {
+    return;
+  }
+  return x * 2;
+};
+
+//型アサーション
+const foo2 = {} as { bar: number };
+foo2.bar = 1;
+
+export const Home: NextPage = () => {
+  return <div>Hello</div>;
+};

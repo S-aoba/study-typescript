7100f6e2df066ca14d09c9d1b8182a6ffff5e774
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-01-24 23:04:50 +0900

    🚀 Index Signature Mapped Types

--- index.ts
+++ index.ts
@@ -1,8 +1,35 @@
-export const color = 'red';
+// Index Signature 多用はしない方がいい
+export type User = {
+  name: string;
+  age: number;
+  [key: string]: string | number | undefined;
+};
 
-// const assertion
-const theme = {
-  color: 'red',
-  backgroundColor: 'blue',
-} as const;
+const user: User = {
+  name: 'aoba',
+  age: 10,
+  account: 'aoba__S',
+};
+user.account;
 
+// Mapped Types
+export type User1 = {
+  name: string;
+} & OptionalPersonalData;
+
+type PersonalData = {
+  height: number;
+  weight: number;
+  a: string;
+};
+
+type OptionalPersonalData = {
+  [K in keyof PersonalData]-?: PersonalData[K];
+};
+
+const user1: User1 = {
+  name: 'aoba',
+  height: 10,
+  weight: 10,
+  a: '',
+};

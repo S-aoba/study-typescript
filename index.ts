3d605317decea49ae676caddb7fb94d176a8fac9
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-01-24 22:41:16 +0900

    🎨 const assertion

--- index.ts
+++ index.ts
@@ -1,19 +1,8 @@
-// typeof オブジェクトに対して使用する
-export function double(x: number | string) {
-  if (typeof x === 'string') return Number(x) * 2;
-  return x * 2;
-}
+export const color = 'red';
 
-// keyof 型定義に対して使用する
-export const Obj = {
-  123: 'foo',
-  bar: 'bar',
-};
+// const assertion
+const theme = {
+  color: 'red',
+  backgroundColor: 'blue',
+} as const;
 
-type Key = keyof typeof Obj;
-const key: Key = 123;
-
-function test(x: keyof typeof Obj) {
-  return;
-}
-test()

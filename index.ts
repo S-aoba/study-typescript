1c25f0a80d6c3734e2575674104cf2cd52e65dec
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-01-25 11:13:55 +0900

    📌 関数のGenerics

--- index.ts
+++ index.ts
@@ -1,32 +1,7 @@
-// Generics
-
-export type Foo<T extends string | number = string> = {
-  value: T;
+// 関数のGenerics
+const foo = <T extends string>(arg: T) => {
+  return { val: arg };
 };
 
-const foo1: Foo<string> = {
-  value: '',
-};
-const foo2: Foo<number> = {
-  value: 111,
-};
-// type Japanese = User<'東京都' | '大阪府'>;
-// type American = User<'CA' | 'NY'>;
-
-// const user1: Japanese = {
-//   name: '田中',
-//   state: '東京都',
-// };
-// const user2: American = {
-//   name: 'Johnny',
-//   state: 'CA',
-// };
-// const foo: Foo<string> = {
-//   value: '',
-// };
-// const foo1: Foo<number> = {
-//   value: 0,
-// };
-// const foo2: Foo<boolean> = {
-//   value: true,
-// };
+const foo1 = foo<string>('');
+const foo2 = foo(''); //型推論

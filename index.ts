03deb47bb484ed3cfc8cd1ff307a95dd8f88e513
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-01-25 14:55:26 +0900

    📈 ReturnType Readonly Partial Required Pick Omit

--- index.ts
+++ index.ts
@@ -1,11 +1,31 @@
-type Props = {
-  id: string;
-  name: string;
-  age: number;
+// Utility Types
+
+// ReturnType
+const foo = (id: string, age: number) => {
+  return 0;
 };
 
-type filteredString<T> = {
-  [K in keyof T]: T[K] extends string ? K : never;
-}[keyof T];
+type Foo = ReturnType<typeof foo>;
 
-type StringKeys = filteredString<Props>;
+type User = {
+  name: string;
+  age: number | null;
+  country?: 'US' | 'UK' | 'JP';
+};
+
+// Readonly
+type ReadonlyUser = Readonly<User>;
+// Partial
+type PartialUser = Partial<User>;
+// Required
+type RequiredUser = Required<User>;
+// Pick
+type PickUser = Pick<User, 'name' | 'country'>;
+// Omit
+type OmitUser = Omit<User, 'age'>;
+
+const user: OmitUser = {
+  name: 'aoba',
+  // age: 30,
+  country: 'UK',
+};
